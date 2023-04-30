410bfb66dc4411c86d9bb918e3817c6e638e4cdc
DankGuy <68477088+DankGuy@users.noreply.github.com>
2023-04-30 15:17:47 +0800

    add files

---
+++ Main.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+import "utils/Strings.sol";
+
+contract Main {
+    address[] public userList;
+    address[] public farmList;
+    address[] public DCList;
+    address[] public retailerList;
+    address[] public consumerList;
+
+}
